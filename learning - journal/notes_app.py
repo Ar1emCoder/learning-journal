3d8f5cd0731a51719d7c4d15dd7f047fd24169eb
ArtemCoder <artemtimkin639@gmail.com>
2026-02-07 21:54:01 +0300

    Notes App v3.0Ð: добавлены поиск, статистикау, улучшено рÐедактирование

--- learning - journal/notes_app.py
+++ learning - journal/notes_app.py
@@ -34,7 +34,7 @@ def create_note():
     content = input("Введите текст заметки: ").strip()
 
     # Обработка тегов
-    tags_input = input("Введите теги (через запятую):").strip()
+    tags_input = input("Введите теги (через запятую): ").strip()
     # tags_list = tags_input.split(",") # разделение строки по запятой
     # clean_tags = []
     # for tag in tags_list:
@@ -60,6 +60,63 @@ def get_next_id():
     max_id = max(note['id'] for note in notes)
     return max_id + 1
 
+def delete_note():
+    if not notes:
+        print('Нет заметок для удаления!')
+        return
+
+    show_all_notes()
+    try:
+        delete_id = int(input("Введите ID заметки, для удаления: "))
+    except ValueError:
+        print("Ошибка! Введите число.")
+        return
+    for i, note in enumerate(notes):
+        if delete_id == note['id']:
+            removed_note = notes.pop(i)
+            print(f"Заметка '{note['title']}' {note['id']} удалена")
+            return
+    print(f"Заметка с ID {delete_id} не найдена!")
+
+def edit_note():
+    if not notes:
+        print('Нет заметок для редактирования!')
+        return
+    show_all_notes()
+    try:
+        id_input = int(input("Введите ID заметки для редактирования: "))
+    except ValueError:
+        print("Введите число!")
+        return
+
+    for note in notes:
+        if id_input == note['id']:
+            current_title = note['title']
+            current_content =  note['content']
+            current_tags = note['tags']
+
+            new_title = input(f"Введите новый заголовок заместо [{current_title}](Enter - оставить прежним): ")
+            if not new_title:
+                new_title = current_title
+            preview = current_content[:30] + ("..." if len(current_content) > 30 else "")
+            new_content = input(f"Введите новый текст заместо [{preview}] (Enter - оставить прежним): ").strip()
+            if not new_content:
+                new_content = current_content
+            new_tags = input(f"Введите новый тег заместо [{current_tags}] (Enter - оставить прежним): ")
+            if new_tags:
+                new_tags = [tag.strip() for tag in new_tags.split(",") if tag.strip()]
+            else:
+                new_tags = current_tags
+
+            note['title'] = new_title
+            note['content'] = new_content
+            note['tags'] = new_tags
+            note['updated_at'] = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+            print(f"Заметка ID {note['id']} обновлена")
+            return
+    print(f"Заметка {id_input} не найдена!")
+
 def show_all_notes():
     if not notes:
         print("Пока нет заметок!")
@@ -71,29 +128,74 @@ def show_all_notes():
             print(f"Текст: {note['content']}")
             print('-'*40)
 
+def search_notes():
+    if not notes:
+        print("Нет заметок для поиска!")
+        return
+    search = input("Введите слово для поиска: ").lower().strip()
+    for note in notes:
+        if search in note['title'].lower():
+            print(f"Совпадение в названии заметки с ID: {note['id']} ")
+            return
+        if search in note['content'].lower():
+            print(f"Совпадение в тексте заметки с ID: {note['id']} ")
+            return
+        for tag in note['tags']:
+            if search in tag.lower():
+                print(f"Совпадение в теге заметки с ID: {note['id']} ")
+                return
+    print(f"Слово {search} нигде не найдено!")
+    return
+
+
+def show_stats():
+    if not notes:
+        print("Нет заметок вовсе")
+        return
+    cnt_notes = len(notes)
+    summa = 0
+    for note in notes:
+        summa += len(note['tags'])
+
+    print("="*40)
+    print("\n=== СТАТУС ===")
+    print(f'Количество заметок = {cnt_notes}')
+    print(f'Количество тегов = {summa}')
+    print("="*40)
+
+
 def main():
     global notes
     notes = load_from_json()
     while True:
         print(f"1. Показать все заметки")
         print(f"2. Добавить заметку")
-        print(f"3. Выйти")
-        # print(f"4. Сохранить заметки в файл")
+        print(f"3. Удалить заметку")
+        print(f"4. Редактировать заметку")
+        print(f"5. Выйти")
+        print(f"6. Показать статус")
+        print(f"7. Поиск по слову (заголовок, текст, тег)")
 
-        choice = input("Введите действие (1-3): ")
+        choice = input("Введите действие (1-7): ")
 
         if choice == '1':
             show_all_notes()
         elif choice == '2':
             create_note()
         elif choice == '3':
+            delete_note()
+        elif choice == '4':
+            edit_note()
+        elif choice == '5':
             print("Выход из программы")
             save_to_json()
             break
-        # elif choice == '4':
-        #     save_to_json()
+        elif choice == '6':
+            show_stats()
+        elif choice == '7':
+            search_notes()
         else:
-            print("Неверный выбор! Введите число от 1 до 3")
+            print("Неверный выбор! Введите число от 1 до 7")
 
 if __name__ == "__main__":
     main()
