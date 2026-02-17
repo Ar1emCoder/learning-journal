2cb90aa317aaa9f7a7f1f55bec288ce7f6d4052f
ArtemCoder <artemtimkin639@gmail.com>
2026-02-17 23:02:20 +0300

    Plan for save() method

--- learning - journal/finance_tracker.py
+++ learning - journal/finance_tracker.py
@@ -1,26 +1,50 @@
 from datetime import datetime
 
 class Transaction:
-    def __init__(self, amount, category, opi, type):
+    def __init__(self, amount, category, description, type):
         self.id = None
         self.amount = amount
         self.category = category
-        self.opi = opi
-        self.date = datetime.now()
-        self.type = type # прибыль и расходы
+        self.description = description
+        self.data = datetime.now()
+        self.type = type
 
-    def add_transaction(self, amount, category, discription, type):
-        pass
-
-    def get_balance(self):
-        pass
 class FinanceManager:
     def __init__(self, filename="finance.json"):
-        self.filename = filename
         self.next_id = 1
-        self.type = [] # один список для всех записей расходов и доходов
-        self.load()
+        self.filename = filename
+        self.transactions = []
+        # self.load()
 
+    def add_transaction(self, amount, category, description, type):
+        transaction = Transaction(amount, category, description, type)
+        transaction.id = self.next_id
+        self.transactions.append(transaction)
+        self.next_id += 1
+        # self.save()
 
+    def get_balance(self):
+        balance = 0
+        for transaction in self.transactions:
+            if transaction.type == "income":
+                balance += transaction.amount
+            elif transaction.type == "expense":
+                balance -= transaction.amount
+        return balance
 
+    def show_transactions(self):
+        if not self.transactions:
+            print('Транзакций нет!')
+            return
+        print(f"--- Все транзакции {len(self.transactions)} ---")
+        for transaction in self.transactions:
+            type_rus = "Доход" if transaction.type == "income" else "Расход"
+            print(f"[{transaction.id}] {transaction.amount} руб. | {type_rus} | Категория: {transaction.category}")
+        print("-"*40)
 
+if __name__ == "__main__":
+    fm = FinanceManager()
+    fm.add_transaction(1000, "work", "pay", "income")
+    fm.add_transaction(200, "eat", "products", "expense")
+    print(f"Баланс: {fm.get_balance()}")
+    fm.show_transactions()
