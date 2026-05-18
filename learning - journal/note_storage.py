485bc76fb326f9791a2222edf7ed8e58a9d19120
ArtemCoder <artemtimkin639@gmail.com>
2026-05-18 17:14:35 +0300

    fix: add missing tags field to old notes

--- learning - journal/note_storage.py
+++ learning - journal/note_storage.py
@@ -8,7 +8,11 @@ def load_notes():
         return []
     else:
         with open(FILE_NAME, "r", encoding="utf-8") as f:
-            return json.load(f)
+            notes = json.load(f)
+            for note in notes:
+                if "tags" not in note:
+                    note["tags"] = []
+            return notes
 
 def save_notes(notes):
     with open(FILE_NAME, "w", encoding="utf-8") as f:
